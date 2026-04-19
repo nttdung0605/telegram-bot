c40e1475872dcadfab35ebeadf32b00c18c5bdab
unknown <you@example.comnttdung0605@gmail.com>
2026-04-19 12:57:45 +0700

    fix payment 4.0

--- src/services/telegram.service.js
+++ src/services/telegram.service.js
@@ -1,6 +1,10 @@
 const TelegramBot = require("node-telegram-bot-api");
-const { BOT_TOKEN } = require("../config");
+const { BOT_TOKEN, PAYOS_WEBHOOK_URL } = require("../config");
 
-const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+const bot = new TelegramBot(BOT_TOKEN, { 
+    webHook: {
+      port: PAYOS_WEBHOOK_URL || 3000
+    }
+  });
 
 module.exports = bot;
