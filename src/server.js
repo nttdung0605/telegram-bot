b6fe07c2780683cf1d8e23036035ff2e0bf101c5
unknown <you@example.comnttdung0605@gmail.com>
2026-04-19 13:01:36 +0700

    fix payment 4.0

--- src/server.js
+++ src/server.js
@@ -3,23 +3,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const paymentRoutes = require("./routes/payment.route");
 const bot = require("./services/telegram.service");
-const PAYOS_WEBHOOK_URL = require("../config");
-const BOT_TOKEN = require("../config");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post(`/bot${BOT_TOKEN}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
-
 app.use("/api/payment", paymentRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server chạy tại port ${PORT}`);
-  
-  // Set webhook cho Telegram
-  bot.setWebHook(`${PAYOS_WEBHOOK_URL}/bot${BOT_TOKEN}`);
 });
--- src/services/telegram.service.js
+++ src/services/telegram.service.js
@@ -1,10 +1,9 @@
 const TelegramBot = require("node-telegram-bot-api");
-const { BOT_TOKEN, PAYOS_WEBHOOK_URL } = require("../config");
+const { BOT_TOKEN } = require("../config");
 
 const bot = new TelegramBot(BOT_TOKEN, { 
-    webHook: {
-      port: PAYOS_WEBHOOK_URL || 3000
-    }
+    polling: true,
+    interval: 1000  
   });
 
 module.exports = bot;
